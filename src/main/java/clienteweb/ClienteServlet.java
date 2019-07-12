cafc6a859d27b1e6dd2afcab937ff66de7e1d3e3
Diego Boeira <diegoboeira@live.com>
2019-07-12 20:40:23 -0300

    segundo envio

--- src/main/java/clienteweb/ClienteServlet.java
+++ src/main/java/clienteweb/ClienteServlet.java
@@ -9,9 +9,41 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"/cliente", "/clientServlet", "/clienteController"})
 public class ClienteServlet extends HttpServlet{
+	
+	public ClienteServlet() {
+		System.out.println("Construindo Servlet...");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Inicializando Servlet...");
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Chamando o service...");
+		super.service(req, resp);
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("Chamou pelo método GET");
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String email = req.getParameter("email");
+		
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("confirme email cadastrado: "+ email);
+		System.out.println("Chamou pelo método POST");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("Servlet esta sendo destruido");
+		super.destroy();
+	}
+	
 }
